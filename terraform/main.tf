694d83f90c380f585f5f18f58c28b529888dcde8
Aditya Mittal <amazingadityam@gmail.com>
2026-04-30 00:42:52 +0530

    Grant EC2 read access to AWS and pass TF state location to detector

--- terraform/main.tf
+++ terraform/main.tf
@@ -294,6 +294,11 @@ resource "aws_iam_role_policy_attachment" "ssm" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_role_policy_attachment" "drift_scan" {
+  role       = aws_iam_role.app.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
 resource "aws_iam_instance_profile" "app" {
   name = "drift-demo-app-profile"
   role = aws_iam_role.app.name
